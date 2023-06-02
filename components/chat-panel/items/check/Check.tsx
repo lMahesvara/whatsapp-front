4c40fa126e8aa70b03ed06ab95975f11d790c930
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-01 19:21:47 -0700

    feat: create Check component for the messages

---
+++ components/chat-panel/items/check/Check.tsx
@@ -0,0 +1,14 @@
+import DblCheck from '@/components/svg/DblCheck'
+import React from 'react'
+
+const Check = () => {
+  return (
+    <div className='ml-[3px] inline-block '>
+      <span className='text-[#53bdeb]'>
+        <DblCheck />
+      </span>
+    </div>
+  )
+}
+
+export default Check
