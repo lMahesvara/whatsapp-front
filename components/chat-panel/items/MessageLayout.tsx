a11ec3155cd6dc45ad86a8d1c0888ace78848ec3
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-01 19:20:42 -0700

    feat: create MessageLayout

---
+++ components/chat-panel/items/MessageLayout.tsx
@@ -0,0 +1,28 @@
+type MessageLayoutProps = {
+  marginBottom?: string
+  bgColor?: string
+  children: React.ReactNode
+}
+
+const MessageLayout = ({
+  marginBottom,
+  bgColor = 'bg-[#005c4b]',
+  children,
+}: MessageLayoutProps) => {
+  const styles =
+    bgColor === 'bg-[#005c4b]' ? `rounded-tr-none ` : `rounded-tl-none `
+  const itemsAlign = bgColor === 'bg-[#005c4b]' ? 'items-end' : 'items-start'
+  return (
+    <article
+      className={`pl-6 pr-4 lg:px-[63px] flex items-start flex-col ${marginBottom} ${itemsAlign}`}
+    >
+      <div
+        className={`mb-0 rounded-[7.5px]  max-w-[95%] lg:max-w-[75%] 2xl:max-w-[60%] relative flex-none text-sm text-[#e9edef] pl-[9px] pr-[7px] pb-2 pt-1.5 box-border ${bgColor} ${styles}`}
+      >
+        {children}
+      </div>
+    </article>
+  )
+}
+
+export default MessageLayout
