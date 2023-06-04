435be9c97bd5afd29828d91d55941cd120875ac0
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-04 10:14:58 -0700

    feat: set login as main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
-import ChatCard from '@/components/chatlist-menu/ChatCard'
-import ChatHeader from '@/components/chat-panel/ChatHeader'
-import ChatlistHeader from '@/components/chatlist-menu/ChatlistHeader'
-import ChatFooter from '@/components/chat-panel/footer/ChatFooter'
-import ChatBody from '@/components/chat-panel/ChatBody'
-import ChatPanel from '@/components/chat-panel/ChatPanel'
+import LoginPage from '@/components/login/LoginPage'
 
 export default function Home() {
   return (
-    <main className='relative max-w-[1600px] 2xl:w-[calc(100%-38px)] 2xl:top-[19px] 2xl:h-[calc(100%-38px)] mx-auto flex min-w-[768px] h-full'>
-      <div className='flex flex-col w-[45%] lg:w-[40%] 2xl:w-1/3 bg-[#101a20] flex-grow overflow-visible min-w-[336.6px]'>
-        <ChatlistHeader />
-        <div className='relative flex-grow overflow-y-auto'>
-          {Array(20)
-            .fill('')
-            .map((_, i) => (
-              <ChatCard key={i} i={i} />
-            ))}
-        </div>
-      </div>
-      <ChatPanel />
+    <main>
+      <LoginPage />
     </main>
   )
 }
