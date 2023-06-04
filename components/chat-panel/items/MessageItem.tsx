da9d62fb78dd4b473018757d73e17468539d8d64
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-04 13:50:43 -0700

    feat: use date format

--- components/chat-panel/items/MessageItem.tsx
+++ components/chat-panel/items/MessageItem.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Check from './check/Check'
 import MessageLayout from './MessageLayout'
 import { Message } from '@/types/Message'
+import { getTimeFormat } from '@/utils/timeFormat'
 
 type MessageItemProps = {
   message?: Message
@@ -11,6 +12,8 @@ type MessageItemProps = {
 
 const MessageItem = ({ message, lastMessage }: MessageItemProps) => {
   const marginBottom = lastMessage ? 'mb-3' : 'mb-0.5'
+
+  const time = getTimeFormat(message?.createdAt as Date)
 
   return (
     <MessageLayout marginBottom={marginBottom}>
@@ -21,12 +24,12 @@ const MessageItem = ({ message, lastMessage }: MessageItemProps) => {
         <span>{message?.text}</span>
         <span className='px-1 pb-0 text-[.6875rem] align-middle invisible inline-flex h-0'>
           <span className='w-[19px] flex-shrink-0 flex-grow-0'></span>
-          <span className='flex-grow-0 flex-shrink-0'>6:34 p. m.</span>
+          <span className='flex-grow-0 flex-shrink-0'>{time}</span>
         </span>
       </div>
       <div className='ml-1 mr-0 z-10 relative float-right -mb-[5px] -mt-2.5'>
         <div className='flex cursor-pointer text-[0.6875rem] whitespace-nowrap items-center h-[15px] leading-[15px]'>
-          <span className='inline-block align-top'>6:34 p. m.</span>
+          <span className='inline-block align-top'>{time}</span>
           <Check />
         </div>
       </div>
--- components/chat-panel/items/ResMessageItem.tsx
+++ components/chat-panel/items/ResMessageItem.tsx
@@ -2,6 +2,7 @@ import TailIn from '@/components/svg/TailIn'
 import React from 'react'
 import MessageLayout from './MessageLayout'
 import { Message } from '@/types/Message'
+import { getTimeFormat } from '@/utils/timeFormat'
 
 type ResMessageItemProps = {
   message?: Message
@@ -11,6 +12,7 @@ type ResMessageItemProps = {
 const ResMessageItem = ({ message, lastMessage }: ResMessageItemProps) => {
   const marginBottom = lastMessage ? 'mb-3' : 'mb-0.5'
   const bgColor = 'bg-[#202c33]'
+  const time = getTimeFormat(message?.createdAt as Date)
 
   return (
     <MessageLayout marginBottom={marginBottom} bgColor={bgColor}>
@@ -20,13 +22,13 @@ const ResMessageItem = ({ message, lastMessage }: ResMessageItemProps) => {
       <div className='relative block break-words whitespace-pre-wrap'>
         <span>{message?.text}</span>
         <span className='px-1 pb-0 text-[.6875rem] align-middle invisible inline-flex h-0'>
-          <span className='flex-grow-0 flex-shrink-0'>6:34 p. m.</span>
+          <span className='flex-grow-0 flex-shrink-0'>{time}</span>
         </span>
       </div>
       <div className='ml-1 mr-0 z-10 relative float-right -mb-[5px] -mt-2.5'>
         <div className='flex cursor-pointer text-[0.6875rem] whitespace-nowrap items-center h-[15px] leading-[15px]'>
           <span className='inline-block align-top text-[#ffffff99]'>
-            6:34 p. m.
+            {time}
           </span>
         </div>
       </div>
