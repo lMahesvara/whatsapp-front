e823515e8d099930bafa1d07f8e3e501a2c307dc
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-16 18:10:47 -0700

    feat: add login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,43 @@
-import LoginPage from '@/components/login/LoginPage'
+'use client'
+import Github from '@/components/svg/Github'
+import Google from '@/components/svg/Google'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data, status } = useSession()
+
+  console.log(data?.user, status)
   return (
     <main>
-      <LoginPage />
+      <div className='relative py-16 bg-transparent'>
+        <div className='container relative px-6 m-auto text-white md:px-12 xl:px-40 md:w-8/12 lg:w-6/12 xl:w-6/12 bg-[#202c33] shadow-xl rounded-xl p-6 sm:p-16'>
+          <h2 className='mb-8 text-3xl font-bold text-white'>Iniciar Sesión</h2>
+          <div className='grid mt-16 space-y-4'>
+            <button
+              className='h-12 px-6 transition duration-300 border-2 border-gray-300 rounded-full group hover:border-[#005c4b] '
+              onClick={() => signIn('google', { callbackUrl: '/chats' })}
+            >
+              <div className='relative flex items-center justify-center space-x-4'>
+                <Google />
+                <span className='block text-sm font-semibold tracking-wide text-white transition duration-300 w-max group-hover:text-[#00a884] sm:text-base'>
+                  Continuar con Google
+                </span>
+              </div>
+            </button>
+            <button
+              className='h-12 px-6 transition duration-300 border-2 border-gray-300 rounded-full group hover:border-[#005c4b] '
+              onClick={() => signIn('github', { callbackUrl: '/chats' })}
+            >
+              <div className='relative flex items-center justify-center space-x-4'>
+                <Github />
+                <span className='block text-sm font-semibold tracking-wide text-white transition duration-300 w-max group-hover:text-[#00a884] sm:text-base'>
+                  Continuar con Github
+                </span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
