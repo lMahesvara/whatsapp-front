fab7f2d5f612b1214434afd081d2e03ce4c3ec8b
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-16 18:35:49 -0700

    feat: add new api services

--- services/api.ts
+++ services/api.ts
@@ -1,21 +1,28 @@
-import { Message } from '@/types/Message'
+import { IMessage } from '@/types/Message'
 
 export const getChat = async (id: string, secId: string) => {
   const res = await fetch(`/api/chats/${id}/${secId}`)
-  const data = await res.json()
-  return data
+  return await res.json()
+}
+
+export const getChats = async (id: string) => {
+  const res = await fetch(`/api/chats/${id}`)
+  return await res.json()
 }
 
 export const getUser = async (id: string) => {
   const res = await fetch(`/api/users/${id}`)
-  const data = await res.json()
+  return await res.json()
+}
 
-  return data
+export const getUsers = async () => {
+  const res = await fetch(`/api/users`)
+  return await res.json()
 }
 
 export const postMessage = async (
-  message: Message
-): Promise<Message | null> => {
+  message: IMessage
+): Promise<IMessage | null> => {
   try {
     const res = await fetch(`/api/messages`, {
       method: 'POST',
@@ -26,6 +33,23 @@ export const postMessage = async (
     })
     const data = await res.json()
     return data.message
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export const readMessages = async (chatId: string, userId: string) => {
+  try {
+    const res = await fetch(`/api/chats/read`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ chatId, userId }),
+    })
+    const data = await res.json()
+    return data.chat
   } catch (error) {
     console.log(error)
     return null
